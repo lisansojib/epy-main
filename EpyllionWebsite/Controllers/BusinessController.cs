f0e9d97a9771ee301d44bf9ff6724388109c4814
Md. Sojib Miya <lisancseru@gmail.com>
2019-12-08 18:21:28 +0600

    Initial Create

---
+++ EpyllionWebsite/Controllers/BusinessController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpyllionWebsite.Controllers
+{
+    public class BusinessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
